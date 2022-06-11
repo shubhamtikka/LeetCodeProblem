31ab18abd4df78f1e75de34dd7e6c82314a6e125
shubhamtikka <t.shubham1910@gmail.com>
2022-06-11 22:22:58 +0530

    first commit- Binary Search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,33 @@
+/*******************
+ * 704. Binary Search
+ * Given an array of integers nums which is sorted in ascending order, and an
+ * integer target, write a function to search target in nums. If target exists,
+ * then return its index. Otherwise, return -1.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * Author : Shubham Tikka
+ ********************/
+
+class BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0)
+            return -1;
+
+        int startIndex, endIndex, midIndex;
+        startIndex = 0;
+        endIndex = nums.length - 1;
+
+        while (startIndex <= endIndex) {
+            midIndex = startIndex + ((endIndex - startIndex) / 2);
+            if (nums[midIndex] == target) {
+                return midIndex; // Target found.
+            } else if (nums[midIndex] < target) {
+                startIndex = midIndex + 1; // Look for the target on right side of array
+            } else { // nums[midIndex] > target
+                endIndex = midIndex - 1; // Look for the target on left side of array
+            }
+        }
+        return -1; // Target not found in array
+    }
+}
---
+++ BinarySearch/BinarySearchTest.java
@@ -0,0 +1,78 @@
+
+/*********************
+ * Test class for 704. Binary Search
+ * 
+ *******************/
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BinarySearchTest {
+    BinarySearch binarySearch = new BinarySearch();
+
+    @Test
+    public void testBinarySearch1() {
+        int[] nums = new int[] {};
+        int target = 1;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", -1, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch2() {
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        int target = 9;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 4, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch3() {
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        int target = 3;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 2, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch4() {
+        int[] nums = new int[] { -1 };
+        int target = -1;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 0, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch5() {
+        int[] nums = new int[] { -1, 0 };
+        int target = 0;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 1, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch6() {
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        int target = -1;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 0, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch7() {
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        int target = 12;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", 5, targetIndex);
+    }
+
+    @Test
+    public void testBinarySearch8() {
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        int target = 13;
+        int targetIndex = binarySearch.search(nums, target);
+        assertEquals("check target", -1, targetIndex);
+    }
+
+}
